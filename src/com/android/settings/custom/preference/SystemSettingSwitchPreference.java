5d310c63ddf79d76133eb7686bc4344a9ed210b7
miju12 <mouryabaruah@gmail.com>
2022-04-21 19:05:10 +0000

    rebrand
    Change-Id: I23fa044f9ea65a1de70aa5adecc2b0045a894509

--- src/com/android/settings/custom/preference/SystemSettingSwitchPreference.java
+++ src/com/android/settings/custom/preference/SystemSettingSwitchPreference.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.custom.preference;
+package com.android.settings.candy.preference;
 
 import android.content.Context;
 import android.util.AttributeSet;
